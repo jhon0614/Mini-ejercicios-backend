cd30e9bdbfe8885b79ca123a470918d0a0476594
Jhon Ricardo Rios Cuervo <jhonri.0614@gmail.com>
2026-05-03 16:49:19 -0500

    integrando session al login

--- 4. login google/app.py
+++ 4. login google/app.py
@@ -1,8 +1,9 @@
 from extensions import db, mail, oauth
 from dotenv import load_dotenv
 load_dotenv()
+
 from config import Config
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,14 @@ app.config.from_object(Config)
 db.init_app(app)
 mail.init_app(app)
 oauth.init_app(app)
+
+google = oauth.register(
+    name='google',
+    client_id=app.config["GOOGLE_CLIENT_ID"],
+    client_secret=app.config["GOOGLE_CLIENT_SECRET"],
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'}
+)
 
 from routes.password import password_bp
 from routes.auth import auth_bp
@@ -21,4 +30,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="localhost")
--- 4. login google/routes/auth.py
+++ 4. login google/routes/auth.py
@@ -1,37 +1,46 @@
-from flask import Blueprint, jsonify
-from extensions import oauth
+from flask import Blueprint, jsonify, session, redirect
+from extensions import oauth, db
 from models import Usuario
-from extensions import db
-import os
+
 
 auth_bp = Blueprint("auth", __name__)
 
-google = oauth.register(
-    name='google',
-    client_id=os.getenv("GOOGLE_CLIENT_ID"),
-    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile'}
-)
-
 @auth_bp.route("/login/google")
 def login_google():
+    google = oauth.create_client('google')
     return google.authorize_redirect(
         redirect_uri="http://localhost:5000/auth/google/callback"
     )
 
 @auth_bp.route("/auth/google/callback")
 def callback():
+    google = oauth.create_client('google')
     token = google.authorize_access_token()
-    user_info = google.parse_id_token(token)
+    user_info = token.get("userinfo")
 
     email = user_info["email"]
+    provider_id = user_info["sub"] #id único google
 
     usuario = Usuario.query.filter_by(email=email).first()
 
     if not usuario:
-        usuario = Usuario(email=email, password="")
+        usuario = Usuario(email=email, password="", provider_id=provider_id)
         db.session.add(usuario)
         db.session.commit()
 
-    return jsonify({"message": "Login con Google", "email": email})
+    #sesión
+    session["usuario_id"] = usuario.id
+    session["email"] = usuario.email
+
+    return redirect("/dashboard") #redirigir a la página principal
+
+@auth_bp.route("/dashboard")
+def dashboard():
+    if "usuario_id" not in session:
+        return redirect("/login")
+    return jsonify({"message": f"Bienvenido {session['email']}"})
+
+@auth_bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
